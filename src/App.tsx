7308805180fa78aa66ec4eb272ba69a7cd976695
ZuleykhaBakhshiyeva <bakhshiyevazuleykha@gmail.com>
2022-12-04 01:02:03 +0400

    eslint and prettier configs are added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
 import './App.css';
-import {Header} from './components/Header/Header'
+import { Header } from './components/Header/Header';
 import { Leatest } from './components/leatest_blog/leatest';
 import { Trending } from './components/trending_product/trending';
+import React from 'react';
 function App() {
   return (
     <div className="App">
-   <Header/>
-   <Trending/>
-   <Leatest/>
+      <Header />
+      <Trending />
+      <Leatest />
     </div>
   );
 }
