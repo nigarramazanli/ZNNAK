bb8e3fa537b243b78cb06b8004c2860fcd262734
ZuleykhaBakhshiyeva <bakhshiyevazuleykha@gmail.com>
2022-12-03 14:17:44 +0400

    project was created

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 import {Header} from './components/Header/Header'
+import { Leatest } from './components/leatest_blog/leatest';
+import { Trending } from './components/trending_product/trending';
 function App() {
   return (
     <div className="App">
    <Header/>
+   <Trending/>
+   <Leatest/>
     </div>
   );
 }
---
+++ src/components/leatest_blog/leatest.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import './leatest.scss'
+import img1 from './photo/img1.png';
+import img2 from './photo/img2.png'
+import img3 from './photo/img3.png';
+
+
+
+export const Leatest =() =>{
+    return(
+        <>
+        
+        </>
+    )
+}
