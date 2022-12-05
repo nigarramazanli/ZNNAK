ed9c4ecafdcf31ff6b8d408a969c80cfdff58d6b
ZuleykhaBakhshiyeva <bakhshiyevazuleykha@gmail.com>
2022-12-05 13:39:35 +0400

    hero add

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import { Footer } from './components/Footer/Footer';
 import { Header } from './components/Header/Header';
+import { Hero } from './components/hero/hero';
 import { Leatest } from './components/leatest_blog/leatest';
 import { Trending } from './components/trending_product/trending';
 import React from 'react';
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <Hero />
       <Trending />
       <Leatest />
       <Footer />
---
+++ src/components/hero/hero.tsx
@@ -0,0 +1,57 @@
+import './hero.scss';
+import armchair from './photo/armchair.png';
+import ellipse1 from './photo/Ellipse60.png';
+import ellipse2 from './photo/Ellipse61.png';
+import ellipse3 from './photo/Ellipse62.png';
+import lamp from './photo/lamp.png';
+import vector from './photo/Vector14.png';
+
+export const Hero = () => {
+  return (
+    <>
+      <div className="hero">
+        <div className="left">
+          <div className="lamp">
+            <img className="lamp-img" src={lamp} alt="" />
+          </div>
+          <div className="ellipse-62">
+            <img className="ellipse-pink" src={ellipse3} alt="" />
+          </div>
+          <div className="shop">
+            <div className="text-box">
+              <p className="first-text">Best Furniture For Your Castle....</p>
+              <h1 className="title">
+                New Furniture Collection
+                <br />
+                Trends in 2020
+              </h1>
+              <p className="second-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Magna in est adipiscing
+                <br />
+                in phasellus non in justo.
+              </p>
+            </div>
+            <button className="pink-button">
+              <p>Shop Now</p>
+            </button>
+          </div>
+        </div>
+        <div className="right">
+          <div className="armchair-blog">
+            <img className="armchair" src={armchair} alt="" />
+            <img className="ellipse-60" src={ellipse1} alt="" />
+            <img className="ellipse-61" src={ellipse2} alt="" />
+            <div className="vector-blue">
+              <img className="vector" src={vector} alt="" />
+              <p className="vector-text">
+                50%
+                <br />
+                off
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
