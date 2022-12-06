393ec9f4e3b9d02ecbca8df74db7df81e000385c
ZuleykhaBakhshiyeva <bakhshiyevazuleykha@gmail.com>
2022-12-06 22:12:59 +0400

    hero and _variables added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import './App.css';
 import { Footer } from './components/Footer/Footer';
 import { Header } from './components/Header/Header';
-import { Leatest } from './components/leatest_blog/leatest';
-import { Trending } from './components/trending_product/trending';
+import { Hero } from './pages/hero/hero';
+import { Leatest } from './pages/leatest_blog/leatest';
+import { Trending } from './pages/trending_product/trending';
 import React from 'react';
 function App() {
   return (
     <div className="App">
       <Header />
+      <Hero />
       <Trending />
       <Leatest />
       <Footer />
