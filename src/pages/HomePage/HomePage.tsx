72af2bba5f0659e6d96610454806223c413ac4dd
cami65 <tarverdiyevkamilinho2@gmail.com>
2022-12-08 22:52:14 +0400

    adding variables

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -2,11 +2,13 @@ import './HomePage.scss';
 import { Hero } from './components/hero/hero';
 import { LeatestBlog } from './components/LeatestBlog/LeatestBlog';
 import { TrendingProduct } from './components/TrendingProduct/TrendingProduct';
+import { Unique } from './components/Unique/Unique';
 
 export const HomePage = () => {
   return (
     <>
       <Hero />
+      <Unique/>
       <TrendingProduct />
       <LeatestBlog />
     </>
