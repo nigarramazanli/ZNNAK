c95df74aafbc34a3d088af3b502be7f1e8a8c862
Nigar Akbarli <nigarakbrl@gmail.com>
2022-12-15 17:46:54 +0400

    FeaturedProducts,LatestProducts,ShopexOffer fixed

---
+++ src/pages/HomePage/components/FeaturedProducts/FeaturedProduct.tsx
@@ -0,0 +1,37 @@
+import lupaplus from './lupaplus.png';
+import qalp from './qalp.png';
+import marketarabasi from './marketarabasi.png';
+
+interface IFeaturedProductsProps {
+  imgSrc: string;
+  title: string;
+  code: string;
+  price: number;
+}
+
+export const FeaturedProduct = (props: IFeaturedProductsProps) => {
+  const { imgSrc, title, code, price } = props;
+  return (
+    <div className="chair-box">
+      <div className="greybox">
+        <img src={imgSrc} alt="" />
+      </div>
+      <div className="info">
+        <h2>Cantilever chair</h2>
+        <div className="colors">
+          <button className="green"></button>
+          <button className="pink"></button>
+          <button className="bluee"></button>
+        </div>
+        <p>{code}</p>
+        <p>${price}.00</p>
+      </div>
+      <div className="icons">
+        <div><img src={marketarabasi} alt="" /></div>
+        <img src={qalp} alt="" />
+        <img src={lupaplus} alt="" />
+      </div>
+      <button className='button'>View Details</button>
+    </div>
+  );
+};
---
+++ src/pages/HomePage/components/FeaturedProducts/FeaturedProducts.tsx
@@ -0,0 +1,62 @@
+import './FeaturedProducts.scss';
+import { FeaturedProduct } from './FeaturedProduct';
+import Rectangle1 from './Rectangle pink.png';
+import Rectangle2 from './Rectangle pinkk.png';
+import chair1 from './chair1.png';
+import chair2 from './chair2.png';
+import chair3 from './chair3.png';
+import chair4 from './chair4.png';
+
+export const FeaturedProducts = () => {
+  const products = [
+    {
+      id: 1,
+      imgSrc: chair1,
+      title: 'Cantilever chair',
+      code: 'Code - Y523201',
+      price: 42,
+    },
+    {
+      id: 2,
+      imgSrc: chair2,
+      title: 'Cantilever chair',
+      code: 'Code - Y523201',
+      price: 49,
+    },
+    {
+      id: 3,
+      imgSrc: chair3,
+      title: 'Cantilever chair',
+      code: 'Code - Y523201',
+      price: 47,
+    },
+    {
+      id: 4,
+      imgSrc: chair4,
+      title: 'Cantilever chair',
+      code: 'Code - Y523201',
+      price: 49,
+    }
+  ];
+  return (
+    <div className="featured-products">
+      <h1>Featured Products</h1>
+      <div className="chairs">
+        {products.map((product, index) => (
+          <FeaturedProduct
+            imgSrc={product.imgSrc}
+            title={product.title}
+            code={product.code}
+            price={product.price}
+          />
+        ))}
+      </div>
+      <div className="pink-rectangles">
+        <img src={Rectangle2} alt="" />
+        <img src={Rectangle1} alt="" />
+        <img src={Rectangle1} alt="" />
+        <img src={Rectangle1} alt="" />
+      </div>
+    </div>
+  );
+};
