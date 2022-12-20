42ebb59b26a6ef6350d728da3fab8533b42c1b54
nigarramazanli <116888051+nigarramazanli@users.noreply.github.com>
2022-12-20 19:32:06 +0300

    product-details description block is added

--- src/pages/ProductDetails/Components/Description/Description.tsx
+++ src/pages/ProductDetails/Components/Description/Description.tsx
@@ -6,7 +6,7 @@ export const Description =()=>{
        <div className='description'>
         <div className='first-line-description'>
             <p>Description</p>
-            <p>Additional Info</p>
+            <p className='additional-information'>Additional Info</p>
             <p>Reviews</p>
             <p>Video</p>
         </div>
@@ -18,19 +18,19 @@ export const Description =()=>{
             <p className='varius-more-details'>More details</p>
             <div className='aliquam'>
                 <img src={descriptionarrow} alt="" />
-            <p className='varius-details-text'>Aliquam dis vulputate vulputate integer sagittis. Faucibus dolor ornare faucibus vel sed et eleifend habitasse amet. Montes, mauris varius ac est bibendum. Scelerisque a, risus ac ante. Velit consectetur neque, elit, aliquet. Non varius proin sed urna, egestas consequat laoreet diam tincidunt. Magna eget faucibus cras justo, tortor sed donec tempus. Imperdiet consequat, quis diam arcu, nulla lobortis justo netus dis. Eu in fringilla vulputate nunc nec. Dui, massa viverr .</p>
+            <p className='varius-details-text'>Aliquam dis vulputate vulputate integer sagittis. Faucibus ds diam arcu, nulla lobortis justo netus dis. Eu in fringilla vulputate nunc nec. Dui, massa viverr .</p>
             </div>
             <div className='aliquam'>
                 <img src={descriptionarrow} alt="" />
-            <p className='varius-details-text'>Aliquam dis vulputate vulputate integer sagittis. Faucibus dolor ornare faucibus vel sed et eleifend habitasse amet. Montes, mauris varius ac est bibendum. Scelerisque a, risus ac ante. Velit consectetur neque, elit, aliquet. Non varius proin sed urna, egestas consequat laoreet diam tincidunt. Magna eget faucibus cras justo, tortor sed donec tempus. Imperdiet consequat, quis diam arcu, nulla lobortis justo netus dis. Eu in fringilla vulputate nunc nec. Dui, massa viverr .</p>
+            <p className='varius-details-text'>Aliquam dis vulputate vulputate integer sagittis. Faucibus ds diam arcu, nulla lobortis justo netus dis. Eu in fringilla vulputate nunc nec. Dui, massa viverr .</p>
             </div>
             <div className='aliquam'>
                 <img src={descriptionarrow} alt=""/>
-            <p className='varius-details-text'>Aliquam dis vulputate vulputate integer sagittis. Faucibus dolor ornare faucibus vel sed et eleifend habitasse amet. Montes, mauris varius ac est bibendum. Scelerisque a, risus ac ante. Velit consectetur neque, elit, aliquet. Non varius proin sed urna, egestas consequat laoreet diam tincidunt. Magna eget faucibus cras justo, tortor sed donec tempus. Imperdiet consequat, quis diam arcu, nulla lobortis justo netus dis. Eu in fringilla vulputate nunc nec. Dui, massa viverr .</p>
+            <p className='varius-details-text'>Aliquam dis vulputate vulputate integer sagittis. Faucibus ds diam arcu, nulla lobortis justo netus dis. Eu in fringilla vulputate nunc nec. Dui, massa viverr .</p>
             </div>
             <div className='aliquam'>
                 <img src={descriptionarrow} alt=""/>
-            <p className='varius-details-text'>Aliquam dis vulputate vulputate integer sagittis. Faucibus dolor ornare faucibus vel sed et eleifend habitasse amet. Montes, mauris varius ac est bibendum. Scelerisque a, risus ac ante. Velit consectetur neque, elit, aliquet. Non varius proin sed urna, egestas consequat laoreet diam tincidunt. Magna eget faucibus cras justo, tortor sed donec tempus. Imperdiet consequat, quis diam arcu, nulla lobortis justo netus dis. Eu in fringilla vulputate nunc nec. Dui, massa viverr .</p>
+            <p className='varius-details-text'>Aliquam dis vulputate vulputate integer sagittis. Faucibus ds diam arcu, nulla lobortis justo netus dis. Eu in fringilla vulputate nunc nec. Dui, massa viverr .</p>
             </div>
         </div>
        </div>
