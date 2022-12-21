c1361e0d3bcebbd32204d0e0b48f8a523db94409
Anton Efremenkov <anton.efremenkov@itentika.ru>
2022-12-22 00:42:15 +0300

    fix after merge

--- src/pages/ProductDetails/ProductDetails.tsx
+++ src/pages/ProductDetails/ProductDetails.tsx
@@ -1,7 +1,6 @@
 import './ProductDetails.scss';
 import { Description } from './Components/Description/Description';
 import { Playwood } from './Components/Playwood/Playwood';
-import { Description } from './Components/Description/Description';
 
 export const ProductDetails = () => {
   return (
